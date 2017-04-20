7a444c1b13553c15481a9ddaec8039e956f93d8a
vlad <v_leonov1@mail.ru>
2017-04-20 19:17:09 +0300

    products

--- js/common.js
+++ js/common.js
@@ -1,9 +1,15 @@
 $(document).ready(function() {
   
   //Burger menu
-  $('.burger-wrap a').click(function(){
+  /*$('.burger-wrap a').click(function(){
     $(this).toggleClass('active');
     $('.nav--ul').toggleClass('nav--ul_active')
+    return false;
+  });
+  */
+  $('.burger-wrap').click(function(){
+    $(this).children().toggleClass('active');
+     $('.nav-main').toggleClass('active');
     return false;
   });
   
